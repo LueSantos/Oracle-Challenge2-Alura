841c743ebd7b2bb97e1f730cd64599889f65471c
Luciana Santos <luguys@gmail.com>
2022-06-22 12:27:04 -0300

    layout_v1

---
+++ Rascunho_canvas/jogoDaForca.js
@@ -0,0 +1,57 @@
+/* let tela = document.querySelector('canvas');
+let pincel = tela.getContext('2d');
+
+pincel.fillStyle = 'gray';
+pincel.fillRect(0, 0, 600, 600);
+
+// left eyebrow
+pincel.fillStyle = 'black';
+pincel.fillRect(360, 20, 80, 20);
+
+// right eyebrow
+pincel.fillStyle = 'black';
+pincel.fillRect(160, 20, 80, 20);
+
+// left eye
+pincel.fillStyle = 'black';
+pincel.fillRect(150, 90, 100, 100);
+
+// right eye
+pincel.fillStyle = 'black';
+pincel.fillRect(350, 90, 100, 100);
+
+// center nose
+pincel.fillStyle = 'black';
+pincel.fillRect(230, 170, 140, 100);
+
+
+
+// center "baseline" tongue
+pincel.fillStyle = 'red';
+pincel.fillRect(270, 270, 60, 100);
+
+//iris of the lefth eye
+pincel.fillStyle = 'yellow';
+pincel.arc(200, 140, 20, 0, 2 * 3.14);
+pincel.fill();
+
+//iris of the right eye
+pincel.fillStyle = 'yellow';
+pincel.arc(400, 140, 20, 0, 2 * 3.14);
+pincel.fill();
+
+
+
+/* 
+pincel.fillStyle= "blue";
+pincel.beginPath();
+pincel.moveTo(300,200);
+pincel.lineTo(200,400);
+pincel.lineTo(400,400);
+pincel.fill();
+
+pincel.fillStyle= "yellow";
+pincel.beginPath();
+pincel.arc(300, 200, 50, 0, 2 * 3.14)
+pincel.fill(); */
+ 
